514c199d0025663bd9cd3a09d2af261eea428485
Yegor Bugayenko <yegor256@gmail.com>
2024-01-06 19:36:47 +0300

    year up

--- lib/zache.rb
+++ lib/zache.rb
@@ -2,7 +2,7 @@
 
 # (The MIT License)
 #
-# Copyright (c) 2018-2023 Yegor Bugayenko
+# Copyright (c) 2018-2024 Yegor Bugayenko
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the 'Software'), to deal
@@ -34,7 +34,7 @@
 # {README}[https://github.com/yegor256/zache/blob/master/README.md] file.
 #
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2018-2023 Yegor Bugayenko
+# Copyright:: Copyright (c) 2018-2024 Yegor Bugayenko
 # License:: MIT
 class Zache
   # Fake implementation that doesn't cache anything, but behaves like it
