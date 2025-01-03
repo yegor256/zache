6a26ab38254581cc4565b04cc6df31a568129bb1
Yegor Bugayenko <yegor256@gmail.com>
2025-01-03 12:31:39 +0300

    year up to 2025

--- test/test_zache.rb
+++ test/test_zache.rb
@@ -2,7 +2,7 @@
 
 # (The MIT License)
 #
-# Copyright (c) 2018-2024 Yegor Bugayenko
+# Copyright (c) 2018-2025 Yegor Bugayenko
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the 'Software'), to deal
@@ -32,7 +32,7 @@ Thread.report_on_exception = true
 
 # Cache test.
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2018-2024 Yegor Bugayenko
+# Copyright:: Copyright (c) 2018-2025 Yegor Bugayenko
 # License:: MIT
 class ZacheTest < Minitest::Test
   def test_caches
